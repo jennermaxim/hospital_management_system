188a2e8aecaf1bd950d5bf0acbd20bfa92b1abfb
JennerMaxim <jennersi1remaxim@gmail.com>
2024-01-18 16:11:11 +0300

    Update the App Configuration

--- includes/config.php
+++ includes/config.php
@@ -4,5 +4,9 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'hospital_db');
 
+// define('DB_HOST', 'sql211.ezyro.com');
+// define('DB_USER', 'ezyro_35811796');
+// define('DB_PASSWORD', 'CoDing@6Maxim');
+// define('DB_NAME', 'ezyro_35811796_hms');
+
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-?>
